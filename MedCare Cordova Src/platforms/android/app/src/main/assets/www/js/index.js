3fb657c135cca626041401e5631aeb640b266ee1
Rajan Pandey <rajanpandey2508@gmail.com>
2018-08-29 15:12:05 +0530

    Added the cordova src with apk

---
+++ MedCare Cordova Src/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,41 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        document.addEventListener("offline", onOffline, false);
+        document.addEventListener("online", onOnline, false);
+
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+function onOnline() {
+    
+}
+
+
+function onOffline() {
+   alert('No network connection detected! Switching to Offline Mode!');
+    window.location="offline.html";
+}
+
+app.initialize();
